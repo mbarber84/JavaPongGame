28f374f6268a814962125ff3d941570834000fc7
mbarber84 <mbarber_84@hotmail.co.uk>
2023-05-05 22:57:13 +0100

    Methods Ball Class started

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -8,4 +8,16 @@ public class Ball {
     Ball(){
         
     }
+    public void setXDirection(int randomXDirection){
+        
+    }
+    public void setYDirection(int randomYDirection){
+        
+    }
+    public void move(){
+        
+    }
+    public void draw(Graphics g){
+        
+    }
 }
