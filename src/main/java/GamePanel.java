1b53b6728c004075e4330e9cfdc0af36cda2f387
mbarber84 <mbarber_84@hotmail.co.uk>
2023-05-06 05:55:05 +0100

    GamePanel Run Method Test on simple game loop (print test repeatedly to console)

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class GamePanel extends JPanel implements Runnable{
     
     static final int GAME_WIDTH = 1000;
-    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (5/9));//PingPong table dimentions
+    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));//PingPong table dimentions
     static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
     static final int BALL_DIAMETER = 20;
     static final int PADDLE_WIDTH = 25;
@@ -39,7 +39,10 @@ public class GamePanel extends JPanel implements Runnable{
         
     }
     public void paint(Graphics g){
-        
+        image = createImage(getWidth(),getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image, 0, 0, this);
     }
     public void draw(Graphics g){
         
@@ -51,6 +54,25 @@ public class GamePanel extends JPanel implements Runnable{
         
     }
     public void run(){
+        //basic game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        
+        while(true){
+            long now = System.nanoTime();
+            delta += (now -lastTime) / ns;
+            lastTime = now;
+            
+            if(delta >= 1){
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+                System.out.println("TEST");
+            }
+        }
         
     }
     public class AL extends KeyAdapter{ //AL = Actual Listener
