2655b1a7d72c2a1aee435d05ee87de55750c815f
mbarber84 <mbarber_84@hotmail.co.uk>
2023-05-08 07:48:06 +0100

    Comments for PongGame class

--- src/main/java/PongGame.java
+++ src/main/java/PongGame.java
@@ -3,11 +3,11 @@ import  java.awt.event.*;
 import  java.util.*;
 import javax.swing.*;
 
-public class PongGame {
+public class PongGame {//The PongGame class is declared as public. It will act as the entry point for the game.
     
-    public static void main(String[] args){
+    public static void main(String[] args){ //The main method is the starting point for programs execution.
         
-        GameFrame frame = new GameFrame();
+        GameFrame frame = new GameFrame();//instance of the GameFrame class is created and assigned to the variable frame. The GameFrame class represents the main window of the game.
         
     }
 }
